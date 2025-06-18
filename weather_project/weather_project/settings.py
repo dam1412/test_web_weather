90dcbf67504894fe7dba82e8aafeaf41d9fea8eb
Damdeptrai <adunhamay121@gmail.com>
2025-06-18 17:39:12 +0700

    #

--- weather_project/weather_project/settings.py
+++ weather_project/weather_project/settings.py
@@ -123,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#New update
+
+CSRF_TRUSTED_ORIGINS = ['https://your-domain.com', 'https://www.your-domain.com']
